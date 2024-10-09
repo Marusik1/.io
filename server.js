31f4be80e2775d255ecbef29ac7f2ea7a072291d
Marusik1 <m4rusik01@yandex.ru>
2024-10-09 21:55:02 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,25 @@
 const express = require('express');
 const fs = require('fs');
+const path = require('path');
 const app = express();
-const port = 3000;
+const port = 3000; // Вы можете выбрать любой порт
 
+// Настройка маршрута для загрузки PDF-файлов
 app.get('/api/pdf/:subject', (req, res) => {
   const subject = req.params.subject;
-  const filePath = `./pdf/${subject}.pdf`; // Путь к PDF-файлу на сервере
+  const filePath = path.join(__dirname, 'pdf', ${subject}.pdf); // Путь к PDF-файлу 
 
   try {
     const fileStream = fs.createReadStream(filePath);
     res.setHeader('Content-Type', 'application/pdf');
-    fileStream.pipe(res);
+    fileStream.pipe(res); // Отправка PDF-файла клиенту
   } catch (error) {
-    console.error('Ошибка при чтении PDF:', error);
-    res.status(500).send('Ошибка при загрузке PDF');
+    console.error('Ошибка при загрузке PDF:', error);
+    res.status(500).send('Ошибка при загрузке PDF'); // Ответ об ошибке
   }
 });
 
+// Запуск сервера
 app.listen(port, () => {
-  console.log(`Сервер запущен на порту ${port}`);
+  console.log(Сервер запущен на порту ${port});
 });
